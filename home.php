af2023566b94929928475cb9902295886581380f
shellmingm <ahmadmuthahharikamil@gmail.com>
2025-05-17 14:23:28 +0700

    home.php

---
+++ home.php
@@ -0,0 +1,9 @@
+<div class="card">
+    <div class="card-body">
+        <p>
+            <img src="gambar\app.jpeg" alt="wkw" width="40" height="24">
+            <h1>Aplikasi Nilai Siswa</h1>
+            <h3>Web Programming</h3>
+        </p>
+    </div>
+</div>
